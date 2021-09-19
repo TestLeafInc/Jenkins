74f90efd1d777c130ce6dd3fa533f7543013ffff
svelmurugandgl <68194983+svelmurugandgl@users.noreply.github.com>
2021-09-19 17:49:28 +0530

    Update Login.java

--- BuildAutomation/src/main/java/chrome/Login.java
+++ BuildAutomation/src/main/java/chrome/Login.java
@@ -16,7 +16,7 @@ public class Login {
 
 	@Test
 	public void loginToLeafTaps() {
-                 // Read the param                             
+                 // Read the param added                            
 		String browser = System.getProperty("browser");
 		System.out.print(browser);
 		if(browser == null){
